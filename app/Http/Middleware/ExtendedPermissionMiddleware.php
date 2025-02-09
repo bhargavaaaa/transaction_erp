eb02cb3fb43ac6ad98318060bf705d3d76a5e046
Bhargav Chaniyara <bhargavchaniyara342@gmail.com>
2025-02-09 12:29:26 +0530

    change has been added

--- app/Http/Middleware/ExtendedPermissionMiddleware.php
+++ app/Http/Middleware/ExtendedPermissionMiddleware.php
@@ -26,6 +26,34 @@ class ExtendedPermissionMiddleware
                 }
             }
 
+            if ($request->route()->getName() == "dashboard") {
+                if (isAdmin()) {
+                    return redirect('dashboard');
+                } else if (auth()->user()->can('gantt-view')) {
+                    return redirect('dashboard');
+                } else if (auth()->user()->can('order-process-card-view')) {
+                    return redirect('order-process-card');
+                } else if (auth()->user()->can('order-view')) {
+                    return redirect('order');
+                } else if (auth()->user()->can('cutting-view')) {
+                    return redirect('cutting');
+                } else if (auth()->user()->can('turning-view')) {
+                    return redirect('turning');
+                } else if (auth()->user()->can('milling-view')) {
+                    return redirect('milling');
+                } else if (auth()->user()->can('other-view')) {
+                    return redirect('other');
+                } else if (auth()->user()->can('dispatch-view')) {
+                    return redirect('dispatch');
+                } else if (auth()->user()->can('role-view')) {
+                    return redirect('role');
+                } else if (auth()->user()->can('user-view')) {
+                    return redirect('user');
+                } else {
+                    return redirect('profile');
+                }
+            }
+
             throw UnauthorizedException::forPermissions($permissions);
         } else {
             return $next($request);
